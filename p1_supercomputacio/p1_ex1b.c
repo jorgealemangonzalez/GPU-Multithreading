4473eb71b3ca11c395cfb1a6ca0098d3e8e384cf
jorge <jorge.alemangonzalez@gmail.com>
2016-02-16 14:40:48 +0000

    falta valgrind en ex1

--- p1_supercomputacio/p1_ex1b.c
+++ p1_supercomputacio/p1_ex1b.c
@@ -6,10 +6,13 @@ static const int SIZE = 15;
 
 
 //swap values at memory locations
-void swap(int *elem1, int *elem2)
-{
-
+void swap(int *elem1, int *elem2)//Exange the values stored in the memory spaces elem1 and elem2
+{								 //To do it we create an auxiliar variable where the value of the first element
+								 //is stored while we store the value of the second iin the first one
 //Your code here
+	int aux = *elem1; //We use an auxiliar variable to do the swap
+	*elem1 = *elem2;
+	*elem2 = aux;
 
 }
 
@@ -21,11 +24,15 @@ void bubbleSort(int * const array, const int size)
 
 //Your code here
 
-
 //1. Iterate along array elements
 
 //2. swap adjacent elements if they are out of order
 
+	int i , j;
+	for(i = 0; i < size-1 ; ++i)
+		for(j = 0 ; j < size - i -1 ; ++j)
+			if(*(array + j) > *(array + j + 1))
+				swap(array+j , array+j+1);
 
 
 }
@@ -46,15 +53,20 @@ int main(void)
 	printf("Print original values\n");
 
 	//Your code here
-
+	int i;
+	for(i = 0 ; i < SIZE ; ++i)
+		printf("%d ",*(a+i));
+	printf("\n");
 	//Sort the array
 	//....
-
+	bubbleSort(a,SIZE);
 
 	printf("Print ordered values\n");
 
 	//Your code here
-
+	for(i = 0 ; i < SIZE ; ++i)
+		printf("%d ",*(a+i));
+	printf("\n");
 	return 0;
 
 
