59ee394e3aab6902556f53f507850c957d121669
u114139 <u114139@54003p33.aulesca.upf.edu>
2016-02-25 17:45:36 +0100

    Ejercicio1

--- p2_supercomputacio/p2_vector.cu
+++ p2_supercomputacio/p2_vector.cu
@@ -3,9 +3,9 @@
 
 __global__ void add(int *a, int *b, int *c)
 {
-	int tid = threadIdx.x;
-	c[tid]=a[tid]+b[tid];
-}
+	int tid = threadIdx.x + blockIdx.x * blockDim.x; //El id del thread es el id que tiene ese thread dentro de un bloque
+	c[tid]=a[tid]+b[tid];							 //El id del bloque es el id que tiene ese bloque dentro del grid			
+}													 //La dimension del bloque es el numero de threads que tiene cada bloque
 
 int main()
 {
@@ -25,12 +25,10 @@ int main()
 	cudaMemcpy(dev_a, a, N*sizeof(int), cudaMemcpyHostToDevice); //host to device
 	cudaMemcpy(dev_b, b, N*sizeof(int), cudaMemcpyHostToDevice);
 
-	int blockSize = 1024;
-	int gridSize = (int)ceil((int)N/blockSize);
+	add<<<1,N>>>(dev_a,dev_b,dev_c); //Ejecuta 1 bloque con N threads
+	//add<<<N,1>>>(dev_a,dev_b,dev_c); //Ejecuta N bloques de 1 solo thread cada uno
 	
-	add<<<N,1>>>(dev_a,dev_b,dev_c); //Execute Kernel 1 sol bloc amb tants threads como dimension
 	//Call CUDA kernel
-	
 	cudaMemcpy(c, dev_c, N*sizeof(int), cudaMemcpyDeviceToHost);//Copy memory from device to host
 	//copy array to host
 	for (int i = 0; i < N; i++)
