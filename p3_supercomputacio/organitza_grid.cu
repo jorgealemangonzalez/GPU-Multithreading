446e97ab79760fe5ff1210c04bf50515b41d7c4d
u114188 <u114188@54003p33.aulesca.upf.edu>
2016-03-01 16:40:01 +0100

    no terminado organitza

--- p3_supercomputacio/organitza_grid.cu
+++ p3_supercomputacio/organitza_grid.cu
@@ -10,13 +10,20 @@ static const int N = 16;
 //Kernel que distribueix la l'execució a la grid
 __global__ void organitza_grid(int *array) {
 
-    int idx_x;
-    int idx_y; 
+
 
     //Distribueix la grid(blocks i threads) com a un array unidimensional i calcula l'index d'aquesta distribució. 
     //On cada index correspon a un thread de la grid
-    
-	//....
+    int idx = threadIdx.x;
+    int idy = threadIdx.y;
+    int idblocy = blockIdx.y;
+    int idblocx = blockIdx.x;
+    int width = gridDim.x * blockDim.x;
+    int id_array = idy*width + idx + idblocx * blockDim.x + idblocy * width * blockDim.y;
+    //array [id_array] = (2*idblocy)+idblocx;
+    //array[idblocy*width+idblocx*blockDim.x+idy*blockDim.y+idx] = idblocy*width+idblocx*blockDim.x+idy*blockDim.y+idx;
+	
+    //....
     
      //Recupera l'index del block a la grid
 
@@ -35,7 +42,9 @@ __host__ void printa(int *array)
 {
 
 //Els vostre codi...
-
+    for(int i = 0 ; i < N ; ++i){
+        printf("%d-",array[i]);
+    }
 
 }
 
@@ -43,24 +52,34 @@ __host__ void printa(int *array)
 int main(void) {
 
 
+    //blockDim.x -- number threads in block
+    //blockid -- block index
+    //gridim number blocks in grid
+
 
     int *dev_a;
     int *array;
+    int size = N*sizeof(int);
 
     // Reserva memoria al host i al device
+    array = (int *)malloc(size); 
+
+    cudaMalloc((void **)&dev_a, size); 
+
 
 
     //Crea blocks de dos dimensions amb diferent nombre de threads. Ex: Comença amb 4x4
-    dim3 block_dim;
+    dim3 block_dim(4,4); //4 threads x bloque
 	//...
 
     // Crea i inicialitza una grid en 2 dimensions
-    dim3 grid_dim;
+    //dim3 grid_dim(sqrt(size)/block_dim.x, sqrt(size)/block_dim.y);
+    dim3 grid_dim(2,2);
 	//...
 
-
+    cudaMemset(dev_a,0,N);
     organitza_grid<<<grid_dim, block_dim>>>(dev_a);
-
+    cudaMemcpy(array,dev_a,N,cudaMemcpyDeviceToHost);
 
 
     // Printa els resultats de l'organització de la grid
