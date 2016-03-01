4a1ea282d52e66179ef76de9e92bb357552dd526
u114139 <u114139@54003p32.aulesca.upf.edu>
2016-03-01 15:28:20 +0100

    Error de void pero seguramente terminado

--- p3_supercomputacio/cerca.cu
+++ p3_supercomputacio/cerca.cu
@@ -1,7 +1,9 @@
 #include<stdio.h>
 #include <time.h>
+#include<math.h>
 #define TRUE 1
 #define FALSE 0
+#define MIN(a,b) (a < b?a:b )
 static const int N = 15;
 
 __global__ void cerca_array_device(int *array,int *valor,int *res)
@@ -46,16 +48,31 @@ int main()
 
 
    //Execució a la CPU
+    int res;
     clock_t t_host = clock();
-    cerca_array_host(a,valor);
+    res = cerca_array_host(a,valor);
     t_host = clock() - t_host;
     double time_taken_host = ((double)t_host)/CLOCKS_PER_SEC;
  
     printf("CPU: %f segons \n", time_taken_host);
-
-
+	
+	if(res == TRUE)
+		printf("host: We found the number\n");
+	else
+		printf("host: We don't found the number :(\n");
+	
     clock_t t_device = clock();
-	cerca_array_device<<< 1,N >>>
+    int *dev_array , *dev_value , *dev_res;
+	cudaMalloc((void**)&dev_array, N*sizeof(int) );
+	cudaMalloc((void**)&dev_value, sizeof(int) );
+	cudaMalloc((void**)&dev_res, sizeof(int) );
+	
+    int threads_block = MIN(512,N);
+	while(N%threads_block != 0)--threads_block;
+	int blocks = N / threads_block;
+	
+	cerca_array_device<<<blocks,threads_block>>>(dev_array,dev_value,dev_res); 
+	cudaMemcpy(res, dev_res, sizeof(int), cudaMemcpyDeviceToHost);//Copy memory from device to host
     t_device = clock() - t_device;
 
     double time_taken_device = ((double)t_device)/CLOCKS_PER_SEC; 
@@ -64,7 +81,10 @@ int main()
    
 
 	//Printa si ha cercat el nombre
-	
+	if(res == TRUE)
+		printf("device: We found the number\n");
+	else
+		printf("device:  don't found the number :(\n");
    
 return 0;
 
